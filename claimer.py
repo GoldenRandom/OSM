f66546f981127038bfc164fffd8b451324ce4fdb
GoldenRandom <115477326+GoldenRandom@users.noreply.github.com>
2026-08-02 23:56:53 +0100

    Update logging level to CRITICAL
    Change logging level from INFO to CRITICAL.

--- claimer.py
+++ claimer.py
@@ -42,7 +42,7 @@ def send_whatsapp_message(text):
         log.error(f"Failed to send Green API message: {e}")
 
 # Setup logging
-logging.basicConfig(level=logging.INFO, format="%(asctime)s │ %(levelname)-8s │ %(message)s", datefmt="%H:%M:%S")
+logging.basicConfig(level=logging.CRITICAL, format="%(asctime)s │ %(levelname)-8s │ %(message)s", datefmt="%H:%M:%S")
 log = logging.getLogger("boss_coin_claimer")
 
 BASE_URL = "https://en.onlinesoccermanager.com"
